48d4c1091011cd72cd7fcdfb2ab16f1f3650b5c2
d.zuev <d.zuev@dodopizza.com>
2022-09-20 22:09:21 +0800

    initial solution commit

---
+++ UP.Ates/UP.Ates.TaskTracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UP.Ates.TaskTracker.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
