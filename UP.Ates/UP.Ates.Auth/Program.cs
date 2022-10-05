d955194d54e9a8ef1753a9b6e22324ee6cfc3127
d.zuev <d.zuev@dodopizza.com>
2022-10-05 20:46:07 +0800

    Add auth with hardcoded users and roles

--- UP.Ates/UP.Ates.Auth/Program.cs
+++ UP.Ates/UP.Ates.Auth/Program.cs
@@ -1,6 +1,8 @@
-using Microsoft.AspNetCore.Identity;
+using IdentityServer4;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using UP.Ates.Auth.Data;
+using UP.Ates.Auth.IdentityConfiguration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,9 +12,41 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+// builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//     .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddIdentityServer(options =>
+    {
+        options.Authentication.CookieSameSiteMode = SameSiteMode.None;
+    })
+    .AddInMemoryClients(Clients.Get())
+    .AddInMemoryIdentityResources(Resources.GetIdentityResources())
+    .AddInMemoryApiResources(Resources.GetApiResources())
+    .AddInMemoryApiScopes(Scopes.GetApiScopes())
+    .AddTestUsers(Users.Get())
+    .AddDeveloperSigningCredential();
+
+builder.Services.ConfigureExternalCookie(options =>
+{
+    options.Cookie.IsEssential = true;
+    options.Cookie.SameSite = (SameSiteMode)(-1); //SameSiteMode.Unspecified in .NET Core 3.1
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.IsEssential = true;
+    options.Cookie.SameSite = (SameSiteMode)(-1); //SameSiteMode.Unspecified in .NET Core 3.1
+});
+
+
+builder.Services.Configure<CookieAuthenticationOptions>(IdentityServerConstants.DefaultCookieAuthenticationScheme, options =>
+{
+    options.Cookie.SameSite = SameSiteMode.None;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+    options.Cookie.IsEssential = true;
+});
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -20,6 +54,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
+    app.UseDeveloperExceptionPage();
 }
 else
 {
@@ -27,18 +62,20 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseCookiePolicy();
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthentication();
 app.UseAuthorization();
+app.UseIdentityServer();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
+
+//
+// app.MapControllerRoute(
+//     name: "default",
+//     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
--- UP.Ates/UP.Ates.TaskTracker/Program.cs
+++ UP.Ates/UP.Ates.TaskTracker/Program.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using UP.Ates.TaskTracker.Data;
+using UP.Ates.TaskTracker.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,10 +11,39 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = "Cookies";
+        options.DefaultChallengeScheme = "oidc";
+    })
+    .AddCookie("Cookies")
+    .AddOpenIdConnect("oidc", options =>
+    {
+        options.SignedOutRedirectUri = "http://localhost:7025";
+        options.Authority = "http://localhost:7025";
+        options.ClientId = "oidcMVCApp";
+        options.ClientSecret = "ProCodeGuide";
+        options.ResponseType = "code";
+        options.UsePkce = true;
+        // options.ResponseMode = "query";
+        options.Scope.Add("weatherApi.read");
+        options.SaveTokens = true;
+        options.RequireHttpsMetadata = false;
+    });
+builder.Services.AddSameSiteCookiePolicy();
+
 builder.Services.AddControllersWithViews();
+builder.Services.Configure<CookieAuthenticationOptions>("idsrv", options =>
+{
+    options.Cookie.SameSite = SameSiteMode.None;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -20,7 +51,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
+    app.UseDeveloperExceptionPage();
 }
+
 else
 {
     app.UseExceptionHandler("/Home/Error");
@@ -28,17 +61,17 @@ else
     app.UseHsts();
 }
 
+app.UseCookiePolicy();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthentication();
 app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
 app.MapRazorPages();
-
 app.Run();
