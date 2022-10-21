ef624550a03168322b134ff5a63cf34add30db48
d.zuev <d.zuev@dodopizza.com>
2022-10-21 17:20:26 +0800

    Basic schema validation

--- UP.Ates/UP.Ates.TaskTracker/Domain/PopugTask.cs
+++ UP.Ates/UP.Ates.TaskTracker/Domain/PopugTask.cs
@@ -1,14 +1,25 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace UP.Ates.TaskTracker.Domain;
 
-public class PopugTask
+public class PopugTask : IValidatableObject
+
 {
     public string Id { get; set; }
     public string Title { get; set; }
     public string JiraId { get; set; }
     public string UserId { get; set; }
     public TaskStatus Status { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        char[] UnsupportedTitleChars = { '[', ']' };
+        if (UnsupportedTitleChars.Any(c => Title.Contains(c)))
+            yield return new ValidationResult("square brackets are not for Title field. If you want to set JiraId, put it to the JiraId field");
+    }
 }
 
 public enum TaskStatus
